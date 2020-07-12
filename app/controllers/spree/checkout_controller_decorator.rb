0ac09e125299dda8c90c679b0554d33f2d33bc6c
Oscar Amado <oaamados@gmail.com>
2020-07-12 12:24:52 -0500

    byebug

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -4,6 +4,7 @@ module Spree
     def update
       EpaycoLogger.debug("payment_params_valid? #{payment_params_valid?} && paying_with_mollie? #{payment_params_valid? && paying_with_mollie?}")
       if payment_params_valid? && paying_with_mollie?
+        byebug
         if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
           payment = @order.payments.last
           payment.process!
